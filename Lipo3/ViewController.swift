335855beff3e52f97e622bb547edb665996265af
Hiroki73LS <hayashi_htt@yahoo.co.jp>
2020-12-30 22:13:51 +0900

    Initial Commit

---
+++ Lipo3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lipo3
+//
+//  Created by user on 2020/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
